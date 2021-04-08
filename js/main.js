362a78e717e78357dc48052d80d4dda109280b98
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-04-08 22:57:03 +0200

    create_modernDesigen

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
   $('.single-item').slick({
-     autoplay: true,
-     speed: 1000,
+      autoplay: true,
+      speed: 1000,
       infinite: true,
       slidesToShow: 2,
       slidesToScroll: 2,
       variableWidth: true
-      
-    });
+  });
+
+
+  $(document).ready(function() {
+      var mixer = mixitup('.container');
+  });
